d31d9d75865623828461b0489147f85134594220
Spencer Arrasmith <spencerarrasmith@gmail.com>
2023-10-08 18:46:38 -0600

    A bunch of changes, but not all of them work

--- UDK_RL_Setup.bat
+++ UDK_RL_Setup.bat
@@ -23,7 +23,7 @@ ECHO.
 TIMEOUT /T 1 > NUL
 ECHO You can stop at any point with Ctrl+C or by closing the window
 ECHO.
-ECHO v20210211 > log.txt
+ECHO v20230910 > log.txt
 PAUSE
 ECHO.
 
@@ -33,6 +33,7 @@ REM             Install UDK if it isn't there already
 
 REM ############################################################################
 
+ECHO.
 ECHO STEP 2
 ECHO.
 ECHO Once you're ready we'll start the UDK install . . .
@@ -43,7 +44,7 @@ SET /p udkinstalled="Do you already have UDK installed? (y/n): "
 IF "%udkinstalled%" == "y" (
     ECHO udkinstalled: y >> log.txt
     ECHO.
-    ECHO Fantastic news! Now to make some changes . . .
+    ECHO Fantastic news^^! Now to make some changes . . .
     ECHO.
     GOTO SkipUDKInstaller
     ECHO.
@@ -52,7 +53,6 @@ IF "%udkinstalled%" == "y" (
 :NotUDKInstaller
 ECHO udkinstalled: n >> log.txt
 ECHO.
-
 
 REM Look for the UDK 2015 Installer program and start it
 IF EXIST UDKInstall-2015-02.exe (
@@ -96,16 +96,16 @@ ECHO.
 ECHO 1 - Select Empty Game for the Project type ^(Starter Content will break Rocket League^)
 TIMEOUT /T 1 > NUL
 ECHO.
-ECHO 2 - Type in a Project Name such as RLMM, RLMods, mods, or leave it as Custom
+ECHO 2 - Type in a Project Name with NO SPACES such as RLMM, RLMods, mods, or leave it as Custom
 TIMEOUT /T 1 > NUL
 ECHO.
 ECHO 3 - Make sure the Install Location matches the Project Name
 TIMEOUT /T 1 > NUL
 ECHO.
-ECHO 4 - Ignore the Perforce offers
+ECHO 4 - Ignore the Perforce server and offers
 TIMEOUT /T 1 > NUL
 ECHO.
-ECHO 5 - Continue onward once UDK finishes installing ^(2-5 minutes^). Return to Desktop and/or close UDK after it completely finishes . . .
+ECHO 5 - Continue onward once UDK finishes installing ^(2-5 minutes^). Select "Launch UDK" when it finishes.
 TIMEOUT /T 1 > NUL
 ECHO.
 ECHO udkinstalled: y >> log.txt
@@ -114,6 +114,47 @@ ECHO.
 
 :SkipUDKInstaller
 
+ECHO.
+ECHO STEP 3
+:NotUDK
+ECHO.
+ECHO Locating UDK . . .
+SET "process=UDK.exe"
+SET "program=UDK"
+
+for /f "usebackq delims=" %%p in (`wmic process where "name='%process%'"
+                                   get executablepath /value 2^>nul ^| findstr ^=`) do (
+  set %%p
+)
+
+if defined executablePath (
+  ECHO.
+)^
+else (
+  ECHO.
+  ECHO Please start %program% before continuing
+  ECHO.
+  PAUSE
+  GOTO NotUDK
+)
+
+SET udkdir=
+CALL :getUdkFolder "%executablePath%"
+
+IF EXIST "%udkdir%\Binaries\Win64\UDK.exe" (
+    ECHO udkdir: y >> log.txt
+    ECHO Got it^^!
+    GOTO GotUDK
+) ELSE (
+    REM Retry if it's not there
+    ECHO udkdir: n >> log.txt
+    ECHO "%udkdir%"
+    PAUSE
+    GOTO NotUDK
+)
+
+:GotUDK
+TIMEOUT /T 1 > NUL
 
 REM ############################################################################
 
@@ -121,34 +162,15 @@ REM             Move Dummy Classes into the UDK install folder
 
 REM ############################################################################
 
-ECHO STEP 3
-:NotUDK
-ECHO.
-SET /p udkdir="Enter Install Location, including the Project Name (e.g. C:\UDK\RLMM): "
-ECHO.
-ECHO Looking in %udkdir% . . .
-ECHO.
-
-IF EXIST "%udkdir%\Binaries\Win64\UDK.exe" (
-    ECHO udkdir: y >> log.txt
-    ECHO Got it^^!
-    GOTO GotUDK
-) ELSE (
-    REM Retry if you typed the wrong thing or if it's not there
-    ECHO udkdir: n >> log.txt
-    ECHO Did you type the project name at the end? ^(e.g. \CustomGame^)
-    GOTO NotUDK
-)
-
-:GotUDK
-TIMEOUT /T 1 > NUL
-
-ECHO.
 REM Check for DummyClasses in a few places
-ECHO STEP 4
-:NotClasses
 ECHO.
-ECHO Looking for RL Dummy Classes . . .
+ECHO STEP 4
+ECHO Downloading and unzipping Dummy Classes . . .
+SET URL=https://github.com/RocketLeagueMapmaking/RL-Dummy-Classes/archive/refs/heads/master.zip
+SET ZIP="D:\RocketLeagueMapmaking\UDK RL Setup\RL-Dummy-Classes.zip"
+POWERSHELL -command "(New-Object System.Net.WebClient).DownloadFile('%URL%', '%ZIP%')"
+CSCRIPT //NoLogo Goodies\UnzipArchive.vbs %ZIP% "%~dp0"
+:NotClasses
 ECHO.
 TIMEOUT /T 1 > NUL
 
@@ -188,7 +210,7 @@ IF EXIST "%~dp0\RL-Dummy-Classes\README.md" (
 :GotClasses
 
 REM Copy DummyClasses into the UDK install folder using Robocopy
-
+TIMEOUT /T 1 > NUL
 ECHO Copying Dummy Classes into UDK . . .
 ROBOCOPY "%~dp0\%classesdir% " "%udkdir%\Development\Src " /E /NFL /NDL /NJH /NJS /xf README.md /xd .git
 
@@ -211,7 +233,7 @@ REM Modify DefaultEngine.ini with additions from DummyClasses
 
 ECHO Modifying DefaultEngine.ini . . .
 ECHO.
-pushd %~dp0
+PUSHD %~dp0
 CSCRIPT //NoLogo Goodies\ModifyDefaultEngine.vbs "%udkdir%\UDKGame\Config\DefaultEngine.ini"
 ECHO defaultengineini: y >> log.txt
 TIMEOUT /T 1 > NUL
@@ -220,7 +242,7 @@ REM Modify UDKEngine.ini to fix slow UDK loading
 
 ECHO Modifying UDKEngine.ini . . .
 ECHO.
-pushd %~dp0
+PUSHD %~dp0
 CSCRIPT //NoLogo Goodies\ModifyUDKEngine.vbs "%udkdir%\UDKGame\Config\UDKEngine.ini"
 ECHO udkengineini: y >> log.txt
 TIMEOUT /T 1 > NUL
@@ -235,10 +257,13 @@ REM ############################################################################
 ECHO STEP 6
 
 REM Check for DummyAssets
-
-:NotAssets
+ECHO Downloading and unzipping Dummy Assets . . .
 ECHO.
-ECHO Looking for RL Dummy Assets . . .
+SET URL=https://github.com/RocketLeagueMapmaking/RL_DummyAssets/archive/refs/heads/master.zip
+SET ZIP="D:\RocketLeagueMapmaking\UDK RL Setup\RL_DummyAssets.zip"
+POWERSHELL -command "(New-Object System.Net.WebClient).DownloadFile('%URL%', '%ZIP%')"
+CSCRIPT //NoLogo Goodies\UnzipArchive.vbs %ZIP% "%~dp0"
+:NotAssets
 ECHO.
 
 SET assetsdir=
@@ -270,7 +295,7 @@ IF EXIST "%~dp0\RL_DummyAssets\README.md" (
     ECHO dummyassets: n >> log.txt
     ECHO Folder not found. Please download it to %~dp0 and unzip it . . .
     ECHO.
-    START /Wait "" "https://github.com/Martinii89/RL_DummyAssets"
+    START /Wait "" "https://github.com/RocketLeagueMapmaking/RL_DummyAssets"
     PAUSE
     GOTO NotAssets
 )
@@ -279,7 +304,7 @@ IF EXIST "%~dp0\RL_DummyAssets\README.md" (
 
 
 REM Copy DummyAssets into the UDK folder using Robocopy
-
+TIMEOUT /T 1 > NUL
 ECHO Copying Dummy Assets into UDK . . .
 ROBOCOPY "%~dp0\%assetsdir% " "%udkdir%\UDKGame\Content\DummyAssets " /E /NFL /NDL /NJH /NJS /xf README.md /xd .git
 ECHO.
@@ -290,23 +315,14 @@ REM Ask for RL install location
 
 ECHO.
 :SteamOrEpic
-
 SET /p steamepic="Steam or Epic Launcher (s/e): "
-IF "%steamepic%" == "s" (
-    ECHO.
-    ECHO In Steam, right click Rocket League ^> Properties ^> Local Files ^> Browse Local Files
-    ECHO.
-    ECHO Copy the directory and paste it here ^(e.g. C:\Program Files ^(x86^)\Steam\steamapps\common\rocketleague^) . . .
-) ELSE (
-    ECHO.
-    ECHO Copy the directory and paste it here ^(e.g. C:\Program Files\Epic Games\rocketleague^) . . .
-)
 
 :NotRL
 ECHO.
-SET rldir=
-SET /p rldir="Enter Rocket League Install Location: "
-ECHO.
+set "process=RocketLeague.exe"
+set "program=Rocket League"
+CALL :fGetExecutablePath
+CALL :fSetRLDir "%executablePath%"
 ECHO Looking in %rldir% . . .
 ECHO.
 IF EXIST "%rldir%\Binaries\Win64\RocketLeague.exe" (
@@ -316,10 +332,6 @@ IF EXIST "%rldir%\Binaries\Win64\RocketLeague.exe" (
 ) ELSE (
     REM Retry if you typed the wrong thing or if it's not there
     ECHO rldir: n >> log.txt
-    ECHO Not there.
-    IF "%steamepic%" == "s" (
-        ECHO Try Steam ^> Right click RL ^> Properties ^> Local Files ^> Browse Local Files
-    )
     GOTO NotRL
 )
 
@@ -406,7 +418,6 @@ CSCRIPT //NoLogo Goodies\CreateShortcut.vbs "%udkrootdir%\UDK Maps.lnk" "%udkdir
 ECHO mapsshortcut: y >> log.txt
 
 TIMEOUT /T 1 > NUL
-
 
 ECHO Creating UtopiaOverwrite Script . . .
 ECHO.
@@ -535,8 +546,8 @@ PAUSE
 ECHO.
 ECHO Let's launch UDK^^!
 
-REM This is the end :'(
-:TheEnd
+REM Start UDK
+:UDKFrontEnd
 ECHO.
 CD /D "%udkdir%\Binaries"
 START UDKLift.exe editor
@@ -544,4 +555,12 @@ ECHO.
 ECHO gg
 ECHO gg: y >> log.txt
 ECHO.
+
+REM This is the end :'(
+:TheEnd
 PAUSE
+EXIT
+
+:getUdkFolder
+SET folder=%~dp1
+SET "udkdir=%folder%"
